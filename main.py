713fcb6a8e202f8d960470d5a15c947aa3d58e67
Ketan Chowdhury <96525685+Anvilator-web@users.noreply.github.com>
2022-06-21 12:56:33 +0530

    Add rich module to main.py

--- main.py
+++ main.py
@@ -2,26 +2,29 @@ import os
 
 from dotenv import load_dotenv
 from functions.sleep import *
+import rich
 
 VERSION = 1.0
 load_dotenv()
 
-print("Twitter Video Maker Bot")
-print("version", VERSION)
+rich.print("[bold cyan]Twitter[/bold cyan] Video Maker Bot")
+rich.print("version", VERSION)
 
 sleep_3()
 
 def load_env_variables():
-    print('Checking .env file for API Keys...')
+    rich.print('Checking .env file for API Keys...')
 
     twitter_api_key = os.getenv("TWITTER_API_KEY")
     twitter_secret = os.getenv("TWITTER_SECRET")
     twitter_bearer_token = os.getenv("TWITTER_BEARER_TOKEN")
 
     sleep_2()
-    print('API Keys loaded')
+    rich.print('API Keys loaded')
 
 load_env_variables()
+
+
 
 
 
