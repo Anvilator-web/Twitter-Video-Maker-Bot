55e6fb1860e9192ffd5edf8c66e0ca08e1aeef61
Ketan Chowdhury <96525685+Anvilator-web@users.noreply.github.com>
2022-06-21 13:08:18 +0530

    Created a nice box print to print the text

--- main.py
+++ main.py
@@ -12,15 +12,25 @@ rich.print("version", VERSION)
 
 sleep_3()
 
+print("")
+print("")
+print("")
+
+
 def load_env_variables():
-    rich.print('Checking .env file for API Keys...')
+    rich.print('|--------------------------------------------------------------------|')
+    rich.print('|Checking .env file for API Keys...                                  |')
+    rich.print('|____________________________________________________________________|')
 
     twitter_api_key = os.getenv("TWITTER_API_KEY")
     twitter_secret = os.getenv("TWITTER_SECRET")
     twitter_bearer_token = os.getenv("TWITTER_BEARER_TOKEN")
 
     sleep_2()
-    rich.print('API Keys loaded')
+
+    rich.print('|--------------------------------------------------------------------|')
+    rich.print('|[green]API Keys loaded[/green]✔️                                                    |')
+    rich.print('|____________________________________________________________________|')
 
 load_env_variables()
 
