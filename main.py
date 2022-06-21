fb95473461936779760536923709640e930bbb78
Ketan Chowdhury <96525685+Anvilator-web@users.noreply.github.com>
2022-06-21 12:44:08 +0530

    Fixed the functions finally
    load_env_variables is working

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os
 
 from dotenv import load_dotenv
 from functions.sleep import *
-from functions.load_keys import load_env_variables
 
 VERSION = 1.0
 load_dotenv()
@@ -12,9 +11,17 @@ print("version", VERSION)
 
 sleep_3()
 
+def load_env_variables():
+    print('Checking .env file for API Keys...')
+
+    twitter_api_key = os.getenv("TWITTER_API_KEY")
+    twitter_secret = os.getenv("TWITTER_SECRET")
+    twitter_bearer_token = os.getenv("TWITTER_BEARER_TOKEN")
+
+    sleep_2()
+    print('API Keys loaded')
+
 load_env_variables()
-
-
 
 
 
